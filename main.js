5b903f5ea4341fae086af22e36a4a7adccffbe4a
RanvirSharma <77488564+RanvirSharma@users.noreply.github.com>
2021-04-08 16:09:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+var canvas=new fabric.Canvas("myCanvas");
+playerx=10;
+playery=10;
+block_height=30;
+block_width=30;
+var playerimg="";
+var blockimg="";
+function playerupdate(){
+    fabric.Image.fromURL("player.png" ,function(Img){
+        playerimg=Img;
+        playerimg.scaleToWidth(150);
+        playerimg.scaleToHeight(140);
+        playerimg.set({
+            top:playerx,
+            left:playery
+
+        });
+        canvas.add(playerimg);
+    });
+}
+function blockupdate(get_image){
+    fabric.Image.fromURL(get_image ,function(Img){
+        blockimg=Img;
+        blockimg.scaleToWidth(block_width);
+        blockimg.scaleToHeight(block_height);
+        blockimg.set({
+            top:playerx,
+            left:playery
+
+        });
+        canvas.add(blockimg);
+    });
+}
+window.addEventListener("keydown",mykeydown);
+function mykeydown(e){
+    var keypress=e.keyCode;
+    if (e.shiftKey == true && keypress == '80'){
+        block_width=block_width+10;
+        block_height=block_height+10;
+        document.getElementById("Current_width").innerHTML=block_width;
+        document.getElementById("Current_height").innerHTML=block_height;
+    }
+    if (e.shiftKey == true && keypress == '77'){
+        block_width=block_width-10;
+        block_height=block_height-10;
+        document.getElementById("Current_width").innerHTML=block_width;
+        document.getElementById("Current_height").innerHTML=block_height;
+    }
+    if(keypress == '38') { up(); }
+    if(keypress == '40') { down(); }
+    if(keypress == '37') { left(); }
+    if(keypress == '39') { right(); }
+    if(keypress == '87') { blockupdate('wall.jpg'); }
+    if(keypress == '71') { blockupdate('ground.png'); }
+    if(keypress == '76') { blockupdate('light_green.png'); }
+    if(keypress == '84') { blockupdate('trunk.jpg'); }
+    if(keypress == '82') { blockupdate('roof.jpg'); }
+    if(keypress == '89') { blockupdate('yellow_wall.png'); }
+    if(keypress == '68') { blockupdate('dark_green.png'); }
+    if(keypress == '67') { blockupdate('cloud.jpg'); }
+}
